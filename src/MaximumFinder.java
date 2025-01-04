a6990908da3646d7690374dbafc39d17f8f5eb3d
Courage003 <rastogidhruv28@gmail.com>
2025-01-04 21:39:21 +0530

    Extension of max method

--- src/MaximumFinder.java
+++ src/MaximumFinder.java
@@ -1,47 +1,55 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
+
 public class MaximumFinder<T extends Comparable<T>> {
-    private T v1;
-    private T v2;
-    private T v3;
+    private T[] values;
 
-    public MaximumFinder(T v1, T v2, T v3) {
-        this.v1 = v1;
-        this.v2 = v2;
-        this.v3 = v3;
+    //Parameterized constructor for multiple values
+    @SafeVarargs
+    public MaximumFinder(T... values) {
+        this.values=values;
+
     }
 
-    //Static Generic method to find the maximum of three values
-    public static <T extends Comparable<T>> T testMaximum(T v1, T v2, T v3){
-        T max=v1;
-        //Assuming v1 is the maximum
+    //Generic method to find the maximum of three values
+    public static <T extends Comparable<T>> T testMaximum(T... values) {
+        if (values == null || values.length == 0) {
+            throw new IllegalArgumentException("At least one value must be provided.");
+        }
 
-        if(v2.compareTo(max)>0){
-            max=v2;
-        }
-        if(v3.compareTo(max)>0){
-            max=v3;
-        }
-        return max;
+        //Convert the array to a list and sort it
+        List<T> valueList = Arrays.asList(values);
+        Collections.sort(valueList);
+
+        //The maximum value will be the last element after sorting
+        return valueList.get(valueList.size() - 1);
     }
+
 
     // Instance method to call the static testMaximum method using instance variables
     public T testMaximum() {
-        return testMaximum(v1, v2, v3);
+        return testMaximum(values);
     }
 
-    public static void main(String[] args){
-        // Test with Integers
-        System.out.println("Find Maximum of Three Integers");
-        MaximumFinder<Integer> intFinder = new MaximumFinder<>(3, 2, 1);
+    public static void main(String[] args) {
+        // Test with more than three Integers
+        System.out.println("Find Maximum of Multiple Integers");
+        System.out.println("Maximum: " + testMaximum(3, 2, 1, 5, 4));
+
+        // Test with more than three Floats
+        System.out.println("\nFind Maximum of Multiple Floats");
+        System.out.println("Maximum: " + testMaximum(1.1f, 2.2f, 3.3f, 4.4f, 5.5f));
+
+        // Test with more than three Strings
+        System.out.println("\nFind Maximum of Multiple Strings");
+        System.out.println("Maximum: " + testMaximum("Apple", "Peach", "Banana", "Mango", "Cherry"));
+
+        // Test with an instance of MaximumFinder
+        System.out.println("\nUsing MaximumFinder Instance");
+        MaximumFinder<Integer> intFinder = new MaximumFinder<>(1, 2, 3, 4, 5);
         System.out.println("Maximum: " + intFinder.testMaximum());
-
-        // Test with Floats
-        System.out.println("\nFind Maximum of Three Floats");
-        MaximumFinder<Float> floatFinder = new MaximumFinder<>(3.3f, 2.2f, 1.1f);
-        System.out.println("Maximum: " + floatFinder.testMaximum());
-
-        // Test with Strings
-        System.out.println("\nFind Maximum of Three Strings");
-        MaximumFinder<String> stringFinder = new MaximumFinder<>("Apple", "Peach", "Banana");
-        System.out.println("Maximum: " + stringFinder.testMaximum());
     }
 }

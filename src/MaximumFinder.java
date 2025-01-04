440f56c51230c980ec99eb9b9fecfbca3e74f712
Courage003 <rastogidhruv28@gmail.com>
2025-01-04 21:09:24 +0530

    Comparing three floats using compareTo()

--- src/MaximumFinder.java
+++ src/MaximumFinder.java
@@ -1,7 +1,7 @@
 public class MaximumFinder {
     //Method to find maximum of three integers
-    public static Integer testMaximum(Integer v1, Integer v2, Integer v3){
-        Integer max=v1;
+    public static Float testMaximum(Float v1, Float v2, Float v3){
+        Float max=v1;
         //Assuming v1 is the maximum
 
         if(v2.compareTo(max)>0){
@@ -15,8 +15,8 @@ public class MaximumFinder {
 
     public static void main(String[] args){
         System.out.println("Find Maximum of Three Integers");
-        System.out.println("Maximum: "+ testMaximum(3,2,1));
-        System.out.println("Maximum: "+ testMaximum(1,3,2));
-        System.out.println("Maximum: "+ testMaximum(1,2,3));
+        System.out.println("Maximum: "+ testMaximum(3.3f,2.2f,1.1f));
+        System.out.println("Maximum: "+ testMaximum(1.1f,3.3f,2.2f));
+        System.out.println("Maximum: "+ testMaximum(1.1f,2.2f,3.3f));
     }
 }
